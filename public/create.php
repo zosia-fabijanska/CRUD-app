a885a134db6c2156d3146e94ea58211270b07758
Zosia Fabijanska <zosia@good.work>
2018-04-03 14:36:51 +1000

    add form to create and read, add base style sheet

--- public/create.php
+++ public/create.php
@@ -8,6 +8,11 @@
 	<input type="text" name="author" id="author">
     <label for="genre">Genre</label>
 	<input type="text" name="genre" id="genre">
+    <label for="language">Langauge</label>
+	<input type="text" name="langauge" id="language">
+    <label for="published">Published</label>
+	<input type="text" name="published" id="published">
+    <input type="submit" name="submit" value="Add Book">
 </form>
 
 <a href="index.php">Return to Wishlist</a>
--- public/read.php
+++ public/read.php
@@ -1,2 +1,13 @@
 <?php include "templates/header.php"; ?>
+
+<h1>Find your favourite book by title:</h1>
+
+<form method="post">
+	<label for="title">Title</label>
+	<input type="text" id="title" name="title">
+	<input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Back to home</a>
+
 <?php include "templates/footer.php>"; ?>
