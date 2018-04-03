37cad292254288c8cee0705b276a0eae21a908d0
Zosia Fabijanska <zosia@good.work>
2018-04-03 13:49:36 +1000

    index.php added

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Simple Database App</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<h1>Book List</h1>
+
+	<ul>
+		<li><a href="create.php"><strong>Create</strong></a> - add a book</li>
+		<li><a href="read.php"><strong>Read</strong></a> - find a book</li>
+	</ul>
+
+</body>
+</html>
