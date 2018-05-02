70c937a55f2e9bcdc359a3f096410d479c32e470
Zosia Fabijanska <zosia@good.work>
2018-05-02 11:42:04 +1000

    Add check to make sure that fields are not empty when creating a new book

--- public/create.php
+++ public/create.php
@@ -1,55 +1,70 @@
 <?php 
+    require "config.php";
 // When the submit button is clicked
 if (isset($_POST['submit'])) {
 
     //try the following
    try {
+       //fields are all required
+       $required = array('title', 'author', 'genre', 'lang', 'published');
+
+       //loop over each field in the array to check if empty
+       $error = false;
+       foreach($required as $field) {
+            if (empty($_POST[$field])) {
+                $error = true;
+            }
+        }
+       
+        if ($error) {
+            echo "You need to fill out all the required fields to enter a book";
+          }
+        else {
+       
+           // Create connection
+           $conn = new mysqli($host, $username, $password, $dbname);
+           // Check connection
+           if ($conn->connect_error) {
+               die("Connection failed: " . $conn->connect_error);
+           } 
+       
+           //Store form inputs
+           $title = $_POST['title'];
+           $author = $_POST['author'];
+           $genre = $_POST['genre'];
+           $lang = $_POST['lang'];
+           $published = $_POST['published'];
+       
+           //Insert input values into database columns
+           $sql = "INSERT INTO books (title, author, genre, lang, published)
+           VALUES ('$title', '$author', '$genre', '$lang', '$published')";
+       
+           //Print result of query at the top of screen
+           if ($conn->query($sql) === TRUE) {
+               ?>
+               <h3><?php echo $_POST['title']; ?> successfully added.</h3>
+               <?php
+           } else {
+               ?>
+           <h3><?php echo "Error: " . $sql . "<br>" . $conn->error; ?></h3>
+               <?php 
+           }
+       
+           $conn->close();
+       
+           }
+       }
      
-    //require stored mySQL info
-    require "config.php";
 
-
-    // Create connection
-    $conn = new mysqli($host, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
-
-    //Store form inputs
-    $title = $_POST['title'];
-    $author = $_POST['author'];
-    $genre = $_POST['genre'];
-    $lang = $_POST['lang'];
-    $published = $_POST['published'];
-
-    //Insert input values into database columns
-    $sql = "INSERT INTO books (title, author, genre, lang, published)
-    VALUES ('$title', '$author', '$genre', '$lang', '$published')";
-
-    //Print result of query at the top of screen
-    if ($conn->query($sql) === TRUE) {
-        ?>
-        <h3><?php echo $_POST['title']; ?> successfully added.</h3>
-        <?php
-    } else {
-        ?>
-    <h3><?php echo "Error: " . $sql . "<br>" . $conn->error; ?></h3>
-        <?php 
-    }
-
-    $conn->close();
-
-    }
-
-    catch(PDOException $error) 
-	{
-		echo $sql . "<br>" . $error->getMessage();
-	}
+        catch(PDOException $error) 
+	    {
+		    echo $sql . "<br>" . $error->getMessage();
+	    }
 }
 
 
 ?>
+
 <?php 
 include "templates/header.php"; 
 ?>
